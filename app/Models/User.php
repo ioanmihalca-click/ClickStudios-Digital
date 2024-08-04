a4a51ef7a93c98aafd1b8c466ed8611ab1716cc1
Ioan "Click" Mihalca <164161041+ioanmihalca-click@users.noreply.github.com>
2024-08-04 19:56:05 +0300

    filament install

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Panel;
 use Illuminate\Notifications\Notifiable;
 use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -45,4 +46,12 @@ class User extends Authenticatable implements FilamentUser
             'password' => 'hashed',
         ];
     }
+
+ 
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return str_ends_with($this->email, 'contact@clickstudios-digital.com');
+    }
+
+
 }
