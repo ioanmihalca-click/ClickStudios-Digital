ea25543134a80261ee20bcd78d9e1890510b879a
Ioan "Click" Mihalca <164161041+ioanmihalca-click@users.noreply.github.com>
2024-08-08 10:00:00 +0300

    frontend evo

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -10,7 +10,7 @@
     <meta property="og:title" content="Blogul Click Studios Digital - Dezvoltare Web, Marketing Digital, SEO">
     <meta property="og:description" content="Descoperiți cele mai noi tendințe și informații din marketingul digital si Web Development pe blogul Click Studios Digital. Rămâneți la curent cu sfaturi despre SEO, dezvoltare web și multe altele.">
     <meta property="og:image" content="https://clickstudios-digital.com/assets/blog-og-image.webp">
-    <meta property="og:url" content="https://clickstudios-digital.com/blog">
+    <meta property="og:url" content="{{ url()->current() }}">>
     <meta property="og:type" content="website">
     <meta property="og:locale" content="ro_RO">
     <meta property="og:site_name" content="Blogul Click Studios Digital">
@@ -38,7 +38,7 @@
     </script>
 
     <title>Blogul Click Studios Digital - Dezvoltare Web, Marketing Digital, SEO</title>
-    <link rel="canonical" href="https://clickstudios-digital.com/blog">
+    <link rel="canonical" href="{{ url()->current() }}">
     <link rel="icon" href="/assets/favicon-click-studios-digital.jpg" type="image/x-icon">
     <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon-click-studios-digital.jpg">
 
@@ -58,15 +58,15 @@
     </script>
 </head>
 
-<body class="font-sans bg-white min-h-screen flex flex-col" x-data="{ scrollToTop: false }" x-on:scroll.window="scrollToTop = window.scrollY > 100">
+<body class="flex flex-col min-h-screen font-sans bg-white" x-data="{ scrollToTop: false }" x-on:scroll.window="scrollToTop = window.scrollY > 100">
     <x-header-nav />
 
-    <main class="flex-grow max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+    <main class="flex-grow px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
         {{ $slot }}
     </main>
 
-    <footer class="bg-gray-100 py-4 text-center text-xs md:text-sm">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <footer class="py-4 text-xs text-center bg-gray-100 md:text-sm">
+        <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
        
             <p class="text-gray-700">
                 &copy; <span x-text="new Date().getFullYear()"></span> <strong>Click Studios Digital</strong>. Toate drepturile rezervate.
@@ -78,10 +78,10 @@
     <button 
         x-show="scrollToTop" 
         @click="window.scrollTo({ top: 0, behavior: 'smooth' })" 
-        class="fixed bottom-4 right-4 p-2 bg-blue-500 text-white rounded-full shadow-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-600 transition-all duration-300"
+        class="fixed p-2 text-white transition-all duration-300 bg-blue-500 rounded-full shadow-lg bottom-4 right-4 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-600"
         aria-label="Scroll to top"
     >
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7" />
         </svg>
     </button>
