14a7156cf892979cb6fb863926868fd3c58a6165
Ioan "Click" Mihalca <164161041+ioanmihalca-click@users.noreply.github.com>
2024-08-11 14:15:02 +0300

    frontend evo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -180,7 +180,7 @@
       <div class="max-w-3xl mx-auto">
         <h2 class="font-roboto-condensed uppercase tracking-[4px] text-center font-normal text-3xl text-gray-900 mb-10 mt-4 md:mt-14 ">Despre noi</h2>
         
-                     <p class="mb-8 text-2xl tracking-wide text-center text-gray-900 md:mb-16 font-volkov">Suntem o agentie de dezvoltare web si marketing digital cu sediul in Baia Mare, Maramures. Oferim o gama completa de servicii de SEO si web design, publicitate online si marketing digital, branding, productie video publicitara si producție muzicala. Ne mandrim cu abilitatea noastră de a combina arta si tehnologia pentru a crea rezultate extraordinare. </p>
+                     <p class="mb-8 text-2xl tracking-wide text-center text-gray-900 md:mb-16 font-volkov">Suntem o agentie de dezvoltare web si marketing digital cu sediul in Baia Mare, Maramures. Oferim o gama completa de servicii de SEO si web design, publicitate online si marketing digital, branding, productie video publicitara si productie muzicala. Avem abilitatea de a combina arta si tehnologia pentru a crea rezultate extraordinare. </p>
 
          
            <div class="flex justify-center mb-48 animate-pulse">
