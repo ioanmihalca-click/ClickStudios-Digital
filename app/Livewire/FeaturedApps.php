c4b4b724ceb266d4a34379992503a3a1afeaad1c
Ioan "Click" Mihalca <164161041+ioanmihalca-click@users.noreply.github.com>
2025-03-10 12:47:31 +0200

    filament

--- app/Livewire/FeaturedApps.php
+++ app/Livewire/FeaturedApps.php
@@ -16,22 +16,8 @@ class FeaturedApps extends Component
             ->take(3)
             ->get();
 
-        // Debug mai detaliat pentru întregul obiect
-        Log::info('Full Featured Apps Object:', [
-            'raw_data' => $featuredApps->toArray(),
-        ]);
-
         $featuredApps = $featuredApps->map(function ($item) {
             $data = $item->toArray();
-
-            // Debug pentru URL
-            Log::info('Processing item:', [
-                'title' => $data['title'],
-                'website_url' => $data['website_url'] ?? null,
-                'has_url' => isset($data['website_url']),
-                'url_empty' => empty($data['website_url']),
-                'url_type' => gettype($data['website_url'] ?? null)
-            ]);
 
             if (!empty($data['technologies'])) {
                 $data['technologies'] = explode(',', $data['technologies']);
