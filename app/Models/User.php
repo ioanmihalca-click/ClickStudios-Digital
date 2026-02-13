aeda1742653479cfc99ad68c84912898d40d7d8f
Ioan "Click" Mihalca <164161041+ioanmihalca-click@users.noreply.github.com>
2026-02-13 02:48:45 +0200

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,10 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Notifications\Notifiable;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
@@ -43,5 +44,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+       public function canAccessPanel(Panel $panel): bool
+    {
+        return str_ends_with($this->email === 'ioanclickmihalca@gmail.com');
     }
 }
